95bbb4c922cfa3f2f8ac999d1707b76941671764
egorkuzn <egorkuzn2002@mail.ru>
2021-10-17 18:31:56 +0800

    build by wsl

---
+++ build/CMakeFiles/MAIN.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/egorkuzn/egorkuzn/vscode_prjcts/RNK/DNK.cpp" "/home/egorkuzn/egorkuzn/vscode_prjcts/RNK/build/CMakeFiles/MAIN.dir/DNK.cpp.o"
+  "/home/egorkuzn/egorkuzn/vscode_prjcts/RNK/RNK.cpp" "/home/egorkuzn/egorkuzn/vscode_prjcts/RNK/build/CMakeFiles/MAIN.dir/RNK.cpp.o"
+  "/home/egorkuzn/egorkuzn/vscode_prjcts/RNK/main.cpp" "/home/egorkuzn/egorkuzn/vscode_prjcts/RNK/build/CMakeFiles/MAIN.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
